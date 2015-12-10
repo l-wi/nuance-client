dac05c92eeda9cf0ad082f64f2104cb15af7b0d4
Tim Steuer <steuertim@gmail.com>
2015-12-10 20:00:33 +0100

    Removed unecessary import

--- src/main/java/de/unisaarland/edutech/nuanceclient/NuanceCredentials.java
+++ src/main/java/de/unisaarland/edutech/nuanceclient/NuanceCredentials.java
@@ -1,7 +1,6 @@
 package de.unisaarland.edutech.nuanceclient;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
